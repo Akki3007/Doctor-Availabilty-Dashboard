ccb88214cfe745e3b55be473c2807d4c0bb4d044
akhil <k.akhilsai99@gmail.com>
2026-05-15 13:29:47 +0530

    Added seperate doctors data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DoctorDashboard from "./components/DoctorDashboard";
 import Home from "./Home";
+import doctors from "./components/data";
 
 function App() {
-
-  const doctors = [
-    {
-      id: 1,
-      name: "Dr. Raj Kumari",
-      specialization: "Cardiologist",
-      experience: "10+ Years Experience",
-      availability: "10:00 AM - 2:00 PM",
-      image:
-        "https://images.unsplash.com/photo-1559839734-2b71ea197ec2",
-    },
-
-    {
-      id: 2,
-      name: "Dr. Priya Sharma",
-      specialization: "Dermatologist",
-      experience: "8+ Years Experience",
-      availability: "12:00 PM - 5:00 PM",
-      image:
-        "https://images.unsplash.com/photo-1594824476967-48c8b964273f",
-    },
-
-    {
-      id: 3,
-      name: "Dr. Arun Kumar",
-      specialization: "Neurologist",
-      experience: "12+ Years Experience",
-      availability: "9:00 AM - 1:00 PM",
-      image:
-        "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d",
-    },
-
-    {
-      id: 4,
-      name: "Dr. katherine",
-      specialization: "Pediatrician",
-      experience: "7+ Years Experience",
-      availability: "4:00 PM - 8:00 PM",
-      image:
-        "https://images.unsplash.com/photo-1651008376811-b90baee60c1f",
-    },
-  ];
 
   return (
     <BrowserRouter>
